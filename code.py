871a87357383550f033eaf05d5e730c3cfeff69b
Moonlight Shinesparkle <bryanvale1107@gmail.com>
2026-03-14 16:27:43 -0700

    Webpage deployed

--- code.py
+++ code.py
@@ -4,14 +4,18 @@ import time
 # Modules
 import keys
 import saving
+import config.server
 
 # Delegate key misc to keys module
 keys.SetupKeys()
 
-saving.PrintLen()
-saving.TestWrite()
+# saving.PrintLen()
+# saving.TestWrite()
+
+config.server.InitSetup()
 
 # Constantly scan for key presses
 while True:
 	keys.ScanRoutine()
+	config.server.PicoServer.poll()
 	time.sleep(0.00075)
---
+++ config/server.py
@@ -0,0 +1,27 @@
+import wifi
+import socketpool
+import microcontroller
+import binascii
+from adafruit_httpserver import GET, Request, Response, Server, FileResponse, POST
+
+UID = microcontroller.cpu.uid
+DecodedUID = binascii.hexlify(UID).decode("ascii")[-4:]
+
+wifi.radio.start_ap(ssid=f"KeyboardConfig-{DecodedUID}", password="KbdConfig")
+
+Pool = socketpool.SocketPool(wifi.radio)
+PicoServer = Server(Pool,"/config")
+
+@PicoServer.route("/",GET)
+def index(request : Request):
+	return FileResponse(request,"index.html","/config")
+
+@PicoServer.route("/Update",POST)
+def UpdateRoutine(request : Request):
+	return Response(request,"Data uploaded to pico!")
+
+def InitSetup():
+	PicoServer.start(str(wifi.radio.ipv4_address_ap),80)
+	print("Server started!\nConnect at:")
+	print(wifi.radio.ipv4_address_ap)
+	print(wifi.radio)
